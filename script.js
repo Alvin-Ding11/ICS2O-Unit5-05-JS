5b95f9dcceb613c162e50f519f24443385dd58b3
AlvinDing11 <alvin.ding@ocsbstudent.ca>
2021-03-18 13:32:01 +0000

    js and html changes

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-let number = 0 
-// This is the variable that is used. By starting at 0 it can be influenced by the textbox and then changed through operations to get the intended result. 
-
+let number = 0
+// This is the variable that is used. By starting at 0 it can be influenced by the textbox and then changed through operations to get the intended result.
 document.getElementById('button').addEventListener('click', area)
-// This simply allows the button to work and kickstart the function. 
+// This simply allows the button to work and kickstart the function.
 function area () {
   number = document.getElementById('text').value
   // This function takes the imput from the textbox and sets it to the value of number.
@@ -11,5 +10,5 @@ function area () {
   number = number * number
   // This is the operation used to find the area for a square by multiply the number by itself.
   document.write(number)
-  // This displays the answer of the operation on the screen as text. 
+  // This displays the answer of the operation on the screen as text.
 }
